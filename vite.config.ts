a99a3efdf6de7ee71cea261f4e176a4b6d5ff865
Ze Yen <zeyenchai@gmail.com>
2025-11-21 17:23:29 +0800

    add env for switching remote_app url

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,39 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    federation({
-      name: "host_app",
-      remotes: {
-        // remote_app: "http://localhost:5001/assets/remoteEntry.js",
-        remote_app: "https://exchange-rate-remote-app.vercel.app/assets/remoteEntry.js",
-      },
-      shared: ["react", "react-dom"],
-    }),
-  ],
-  server: {
-    port: 5000,
-    cors: true,
-    hmr: false,
-    strictPort: true
-  },
-  preview: {
-    port: 5000,
-    cors: true,
-  },
-  build: {
-    modulePreload: false,
-    target: "esnext",
-    minify: false,
-    cssCodeSplit: false,
-  },
-});
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      federation({
+        name: "host_app",
+        remotes: {
+          remote_app: env.VITE_REMOTE_URL,
+        },
+        shared: ["react", "react-dom"],
+      }),
+    ],
+    server: {
+      port: 5000,
+      cors: true,
+      hmr: false,
+      strictPort: true,
+    },
+    preview: {
+      port: 5000,
+      cors: true,
+    },
+    build: {
+      modulePreload: false,
+      target: "esnext",
+      minify: false,
+      cssCodeSplit: false,
+    },
+  };
+});
